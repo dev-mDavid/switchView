08742b093980dca7e3e1d110f7483efefb26d1e1
mDavid <dev.mdavid@gmail.com>
2020-10-14 16:43:35 -0700

    refactor: shared NgModules loading

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
 
+const components = [
+];
 
+const modules = [
+  MatButtonModule,
+  CommonModule,
+  RouterModule,
+];
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [...components],
+  imports: [ ...modules],
+  exports: [...components, ...modules]
 })
 export class SharedModule { }
