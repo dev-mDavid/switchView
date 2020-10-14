068cdf88bf801cfc97888dee18884f8e0c5900ef
mDavid <dev.mdavid@gmail.com>
2020-10-14 15:43:53 -0700

    config: nav-shell to show in the ui

--- src/app/nav-shell/nav-shell.component.ts
+++ src/app/nav-shell/nav-shell.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-shell',
+  selector: 'nav-shell',
   templateUrl: './nav-shell.component.html',
   styleUrls: ['./nav-shell.component.sass']
 })
