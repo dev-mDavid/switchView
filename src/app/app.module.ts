e6540b873325c88f88b82b347c94078018802d52
mDavid <dev.mdavid@gmail.com>
2020-10-14 15:40:16 -0700

    feat: generate nav-shell component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { NavShellComponent } from './nav-shell/nav-shell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavShellComponent
   ],
   imports: [
     BrowserModule,
